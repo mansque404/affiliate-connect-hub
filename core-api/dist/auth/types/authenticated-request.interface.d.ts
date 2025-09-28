12d73ce0f244ff50fecb7b1491e7eca1b87c1400
Vitor Mansque <Vitor Mansque@ILEGRA-FHPHTX3>
2025-09-28 19:18:51 -0300

    chore: Untrack dist folder

--- core-api/dist/auth/types/authenticated-request.interface.d.ts
+++
@@ -1,5 +0,0 @@
-import { Request } from 'express';
-import { UserPayload } from './user-payload.interface';
-export interface AuthenticatedRequest extends Request {
-    user: UserPayload;
-}
--- core-api/dist/auth/types/user-payload.interface.d.ts
+++
@@ -1,5 +0,0 @@
-export interface UserPayload {
-    userId: string;
-    email: string;
-    name: string;
-}
